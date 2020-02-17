18655c9ee617e77d50fe75d4a009590c2b7a7542
unknown <seglifabian@gmail.com>
2020-02-16 18:18:04 -0600

    Adding React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Router from './Router'
+
+const Main = () => (
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
